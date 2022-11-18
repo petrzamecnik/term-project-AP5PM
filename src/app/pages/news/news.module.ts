ecd1afdfee1072bcd2720bae9b5fdee064d52ef8
petrzamecnik <petazamecek@gmail.com>
2022-11-18 22:55:58 +0100

    initial refactor

--- src/app/pages/news/news.module.ts
+++ src/app/pages/news/news.module.ts
@@ -2,10 +2,10 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { NewsPage } from './news.page';
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
-import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { Tab2PageRoutingModule } from './news-routing.module';
 
 @NgModule({
   imports: [
@@ -15,6 +15,6 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     ExploreContainerComponentModule,
     Tab2PageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [NewsPage]
 })
 export class Tab2PageModule {}
